0f10169f30bc8ae394e87538b3b8b70ac71840b4
KrishnamRathi <krishnam.rathi@gmail.com>
2021-06-24 11:10:39 +0530

    line chart fixed

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,15 +1,15 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Line } from 'react-chartjs-2';
 import getDate from '../utils/dateConvert';
 
-function LineChart({data, label}) {
+function LineChart({ data, label }) {
     const [labels, setLabels] = useState([]);
     const [values, setValues] = useState([]);
 
     const convertData = () => {
-        var lb=[], v=[];
+        var lb = [], v = [];
         data.map((d, i) => {
-            if(d.healthField.length!==0){
+            if (d.healthField.length !== 0) {
                 v.push(parseInt(d.healthField));
                 lb.push(getDate(d.timestamp));
             }
@@ -20,12 +20,12 @@ function LineChart({data, label}) {
 
     useEffect(() => {
         convertData();
-    },[])
+    }, [])
 
     const color = (opacity) => {
-        const r = Math.floor(Math.random()*100)%256;
-        const g = Math.floor(Math.random()*100)%256;
-        const b = Math.floor(Math.random()*100)%256;
+        const r = Math.floor(Math.random() * 100) % 256;
+        const g = Math.floor(Math.random() * 100) % 256;
+        const b = Math.floor(Math.random() * 100) % 256;
         return `rgba(${r}, ${g}, ${b}, ${opacity})`;
     }
 
@@ -40,8 +40,15 @@ function LineChart({data, label}) {
                     tension: 0.1,
                     pointRadius: 3,
                     fill: true,
-                    backgroundColor: color(0.5)
+                    backgroundColor: color(0.4)
                 }]
+            }}
+            options={{
+                scales: {
+                    yAxis:{
+                        min: 0
+                    }
+                }
             }}
         />
     )
