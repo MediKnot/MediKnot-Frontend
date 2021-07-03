b7cd28700378d35f563ddf486d13b4f7e08a97b5
KrishnamRathi <krishnam.rathi@gmail.com>
2021-07-03 18:02:23 +0530

    maps added

---
+++ src/components/SearchList.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import '../styles/searchlist.css';
+import '../App.css';
+import { Avatar } from '@material-ui/core';
+import Loader from './Loader';
+
+
+export default function SearchList({ result, setKeyword, keyword, setLati, setLongi, setZoom}) {
+
+  if (!result) return <Loader />
+  else
+    return (
+      <div 
+        className="list shadow" 
+        style={{ backgroundColor: 'white', position: 'absolute', height: '60%', width: '20%', maxWidth: 400, minWidth: 250, top: 0, right: 0, zIndex: 1000, marginTop: '10%', marginRight: '5%', borderRadius: 10 }}
+      >
+        <input className="input-large shadow" placeholder="Search nearby doctors" style={{ width: '100%', alignSelf: 'center', marginTop: 5 }} onChange={(e) => setKeyword(e.target.value)} />
+        <div style={{overflowY: 'scroll'}}>
+          {result.length === 0 ? <h2>No doctors nearby</h2>: null}
+          {result.map((data, i) => {
+            if (data.name.toLowerCase().includes(keyword.toLowerCase()))
+              return (
+                <div className="child row ai-c" key={i} onClick={() => { setLati(data.address.latitude); setLongi(data.address.longitude); setZoom(13) }} >
+                  <Avatar style={{ marginRight: 10 }}>{data.name?.charAt(0)}</Avatar>
+                  <div className="info">
+                    <b style={{fontSize: 15}}>{data.name}</b>
+                    <p className="font">{data.specialization.toString()}</p>
+                  </div>
+                </div>
+              )
+          })}
+        </div>
+
+      </div>
+    );
+}
