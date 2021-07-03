e87ea4b496f740725b4b5479ff5b44bb7dd48dbc
KrishnamRathi <krishnam.rathi@gmail.com>
2021-07-03 13:52:57 +0530

    search by symptoms completed

--- src/components/DiseaseCard.js
+++ src/components/DiseaseCard.js
@@ -8,15 +8,15 @@ function DiseaseCard({ data }) {
         <div style={{ width: '100%', backgroundColor: '#F2A253', borderRadius: 15 }} className="shadow p-1 mv">
             <h3>{data.name}</h3>
             <p>{data.introduction}</p>
-            {data.symptoms.length !==0 ? <div>
+            {data?.symptoms?.length !==0 ? <div>
                 <h4>Symptoms</h4>
-                {data.symptoms.map((symptom, i) => (
+                {data?.symptoms?.map((symptom, i) => (
                     <span key={i}><FiberManualRecordIcon style={{ height: '10', width: '10' }} className="mh" />{symptom}<br /></span>
                 ))}
             </div> : null}
-            {data.cures.length!==0 ? <div>
+            {data?.cures?.length!==0 ? <div>
                 <h4>Cures</h4>
-                {data.cures.map((cure, i) => (
+                {data?.cures?.map((cure, i) => (
                     <span key={i}><FiberManualRecordIcon style={{ height: '10', width: '10' }} className="mh" />{cure}<br /></span>
                 ))}
             </div>: null}
